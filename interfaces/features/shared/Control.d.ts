880551cda5d02095920f0bd37905b12a5f9afa69
Humberto Navarro <kamaii199827@gmail.com>
2022-07-18 06:27:43 +0000

    Removed statement in Control.d.ts
    Removed statement

--- interfaces/features/shared/Control.d.ts
+++ interfaces/features/shared/Control.d.ts
@@ -36,14 +36,3 @@ interface Modifier {
     target: Feature["id"];
     handler: Controllers | { name: "SomeOtherTypeOFModififer" };
 }
-// Commented out to fix statement not allowed error
-// [
-//     {
-//         type: "controller",
-//         handler: {
-//             name: "range",
-//             min: 1,
-//             max: 10,
-//         },
-//     },
-// ];
