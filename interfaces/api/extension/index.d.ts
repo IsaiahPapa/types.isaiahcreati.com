c3008d1b6bac725792fc0110a4462dcf208a4cc3
Isaiah Paparella <clixpapa@gmail.com>
2023-03-28 12:50:19 -0400

    Library/Clustering Update

--- interfaces/api/extension/index.d.ts
+++ interfaces/api/extension/index.d.ts
@@ -15,7 +15,7 @@ export type ExtensionUserAuthJWTPayload = {
     opaque_user_id: string;
     user_id: string;
     channel_id: string;
-    role: string;
+    role: "broadcaster" | "moderator" | "viewer" | "external";
     is_unlinked: boolean;
     pubsub_perms: {
         listen: string[];
