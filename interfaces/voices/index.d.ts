d3fa6a39857a3e14997e40fdc73ef7ad4fb02427
Isaiah Paparella <48810130+IsaiahPapa@users.noreply.github.com>
2023-08-02 11:48:13 +0200

    + AiVoiceSchema
    Added three new types for voices. AiVoiceSchema, PollyVoice, and VoiceFromApi.
    
    PollyVoice is my db polly document schema I use in code, the other one in AWS is the AWS schema

---
+++ interfaces/voices/index.d.ts
@@ -0,0 +1,30 @@
+export type VoiceFromApi = PollyVoice | AiVoiceSchema;
+
+export type PollyVoice = {
+    language: string;
+    required_role: string;
+    name: string;
+    gender: "Male" | "Female";
+    neural: "Yes" | "No";
+    type: "polly";
+    messages_generated?: number;
+};
+
+export type AiVoiceSchema = {
+    type: "ai";
+    model_id: string;
+    name: string;
+    alt: string;
+    category: string[];
+    public: boolean;
+    enabled: boolean;
+    allowed_ids: string[];
+    messages_generated: number;
+    //Featured on voices page
+    featured: boolean;
+
+    audio_sample_url?: string | undefined;
+    image_waveform_url?: string | undefined;
+    image_banner_url: undefined | string;
+    image_thumbnail_url: undefined | string;
+};
